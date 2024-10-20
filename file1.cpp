7fa5798e58d5cad3d30aa94f8071184270d7b236
Men6d656e <men6d656e@gmail.com>
2024-10-20 23:59:03 +0500

    start of c++

---
+++ file1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // pr
+    int num1, num2, num3;
+    num1 = 12;
+    num2 = 12;
+    num3 = num1 + num2;
+    cout << " hello world ";
+    cout << " hello AKASH ";
+    return 0;
+}
