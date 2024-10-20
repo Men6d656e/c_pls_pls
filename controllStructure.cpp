45355d0d394411a85c9aba61c174a22527c12e21
Men6d656e <men6d656e@gmail.com>
2024-10-21 00:02:41 +0500

    finaliy done the practice

---
+++ controllStructure.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // this is a if else
+    // int age;
+    // cout<<"Tell your age"<<endl;
+    // cin>>age;
+    // if(age<18){
+    //     cout<<"You can't come to my party"<<endl;
+    // }
+    // else if(age==18){
+    //     cout<<"You are good!"<<endl;
+    // }
+    // else{
+    //     cout<<"Sorry we do not have a plane for party"<<endl;
+    // }
+
+    // switch 
+    // switch (age)
+    // {
+    // case 18:
+    //     cout<<"your age is 18";
+    //     break;
+    
+    // case 20:
+    //     cout<<"your age is 20";
+    //     break;
+    
+    // case 22:
+    //     cout<<"your age is 22";
+    //     break;
+    // default:
+    //     cout<<"NO special case"<<endl;
+    //     break;
+    // }
+
+    // for loop
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    // while loop
+    // int i = 0;
+    // while (i<=40)
+    // {
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // 
+    int a = 0;
+    do
+    {
+        cout<<a<<"this is a line"<<endl;
+        a++;
+    } while (a<10);
+    
+
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"this is me";
+    int a = 2, b = 5;
+    cout<<"the value of A + B is:"<<a+b<<endl;
+    cout<<"the value of A - B is:"<<a-b<<endl;
+    cout<<"the value of A * B is:"<<a*b<<endl;
+    cout<<"the value of A / B is:"<<a/b<<endl;
+    cout<<"the value of A % B is:"<<a%b<<endl;
+    cout<<"the value of A ++ is:"<<a++<<endl;
+    cout<<"the value of A -- is:"<<a--<<endl;
+    cout<<"the value of -- A is:"<<--a<<endl;
+    cout<<"the value of ++ A is:"<<++a<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
