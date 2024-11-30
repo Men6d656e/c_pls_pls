29cc6259e2301f69a08595e8035bd998498b8d7b
Men6d656e <men6d656e@gmail.com>
2024-11-30 19:50:13 +0500

    pg done

---
+++ pg16.1.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+using namespace std;
+
+class Test {
+    private:
+    int n;
+    public:
+    void in(){
+        cout<<"Enter Number: "<<endl;
+        cin>>n;
+    }
+    void out(){
+        cout<<"The value of n is: "<<n<<endl;
+    }
+};
+
+int main () {
+    Test *ptr;
+    ptr = new Test;
+    ptr ->in();
+    ptr->out();
+    return 0;
+}
